6904072cae8ccf6d569ea25f1691c5b84ffd7226
Dan Lawson <danlawson@gmail.com>
2016-07-26 14:27:05 +0100

    Blog Files

---
+++ scss-setup/assets/gulpfile.js
@@ -0,0 +1,17 @@
+﻿var elixir = require('laravel-elixir');
+
+// Site Config
+var assetsPath = '';
+var publicPath = '../public';
+
+// Elixir Config
+elixir.config.sourcemaps = false;
+elixir.config.assetsPath = '';
+elixir.config.publicPath = publicPath;
+
+elixir(function (mix) {
+    // Main Styles
+    mix.sass([
+        'main.scss'
+    ], publicPath + '/css/main.css');
+});
